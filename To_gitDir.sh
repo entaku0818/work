2e8721ab6896780795bb275573953d4f14382016
遠藤拓弥 <endo@MacBook-Pro-3.local>
2016-10-03 18:55:15 +0900

    fullpage.jsを追加！遷移いけてる

---
+++ To_gitDir.sh
@@ -0,0 +1,1 @@
+cp -p -r /usr/local/var/www/htdocs/work/* /Users/endo/work/
--- php/tamaku-trash-tweet/trash-tweet.php
+++ php/tamaku-trash-tweet/trash-tweet.php
@@ -2,33 +2,9 @@
 
 
         require_once 'twitter-bot/twitter-bot.php'; //関数呼び出しより手前に記述する
-        $imageURL = "/Users/endo/work/php/tamaku-trash-tweet/140314_hozon_H4_tama_asao.jpg";
-        
-        //曜日を取得する
-        date_default_timezone_set("Asia/Tokyo");
-        $datetime = new DateTime();
-        $w = (int)$datetime->format('w');
+        $message = "今日は燃えるゴミです";
+        $imageURL = "140314_hozon_H4_tama_asao.jpg";
 
-        if($w = 0){
-                $message = "今日のゴミは無いです";
-        }elseif($w = 1){
-                $message = "今日は缶/ペットボトル/ビン/電池の日です";
-        }elseif($w = 2){
-                $message = "今日はミックスペーパーの日です。";
-        }elseif($w = 3){
-                $message = "今日は普通ゴミの日です";           
-        }elseif($w = 4){
-                $message = "今日はプラスチックゴミの日です";
-        }elseif($w = 5){
-                $message = "今日のゴミは無いです";       
-        }elseif($w = 6){
-                $message = "今日は普通ゴミの日です";       
-        }
-
-
-
-        
-        
         post_tweet($message, $imageURL);
 
 
